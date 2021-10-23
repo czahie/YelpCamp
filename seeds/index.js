db5a7d2dae5caf3f0319d6ce8de207f3eceb0189
czahie <czahie1996@gmail.com>
2021-10-23 18:19:46 -0400

    YelpCamp basic routes

---
+++ seeds/index.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+import cities from "./cities.js";
+import { places, descriptors } from "./seedHelpers.js";
+import { Campground } from "../models/campgroud.js";
+("../models/campground");
+
+mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+const campsNum = 50;
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < campsNum; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const camp = new Campground({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,
+        });
+        await camp.save();
+    }
+};
+
+seedDB().then(() => {
+    mongoose.connection.close();
+    console.log(`Generated ${campsNum} camps randomly`);
+});
