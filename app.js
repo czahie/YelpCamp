4b3be728b99760182c1751b39c43f350d661cd03
czahie <czahie1996@gmail.com>
2021-10-26 21:45:11 -0400

    Add styles to current pages

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import path, { dirname } from "path";
 import mongoose from "mongoose";
 import { Campground } from "./models/campgroud.js";
 import methodOverride from "method-override";
+import ejsMate from "ejs-mate";
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
     useNewUrlParser: true,
@@ -17,14 +18,14 @@ db.once("open", () => {
 });
 
 const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
-
-const __dirname = dirname(__filename);
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
     res.send("Hello");
--- seeds/index.js
+++ seeds/index.js
@@ -23,9 +23,14 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < campsNum; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
+            image: "https://source.unsplash.com/collection/483251",
+            descripttion:
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi libero nobis, quis iusto maiores minima beatae tempore iste, nihil voluptatum in debitis minus pariatur rem illo similique. Dolorum, nihil dolor!",
+            price,
         });
         await camp.save();
     }
